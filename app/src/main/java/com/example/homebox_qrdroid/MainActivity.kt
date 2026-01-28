b3b47253ab4086b65b2697983ab1b02bb8c0d602
Eric Park <me@ericswpark.com>
2026-01-28 00:13:58 -0500

    fix: allow for period inputs

--- app/src/main/java/com/example/homebox_qrdroid/MainActivity.kt
+++ app/src/main/java/com/example/homebox_qrdroid/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -23,7 +24,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.homebox_qrdroid.ui.theme.HomeboxqrdroidTheme
@@ -52,7 +55,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    var text by remember { mutableStateOf("") }
+    var assetId by remember { mutableStateOf("") }
 
     Column(
         modifier = modifier
@@ -62,10 +65,21 @@ fun MainScreen(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         TextField(
-            value = text,
-            onValueChange = { text = it },
+            value = assetId,
+            onValueChange = { newText ->
+                if (newText.all { it.isDigit() || it == '-' || it == '.' }) {
+                    assetId = newText
+                }
+            },
             label = { Text("Enter asset ID") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged {
+                    if (!it.isFocused) {
+                        assetId = assetId.replace(".", "")
+                    }
+                },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
         Image(
             painter = painterResource(id = android.R.drawable.ic_menu_gallery),
